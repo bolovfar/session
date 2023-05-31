6dc26023f04338155a18db22ebaec94502ad08a3
bolovfar <114148685+bolovfar@users.noreply.github.com>
2023-05-31 11:40:25 +0300

    Add project files.

---
+++ Session_BolovFarkhat_3ISP9-14/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_BolovFarkhat_3ISP9_14
+{
+    internal class Person
+    {
+        private int IdPerson { get; set; }
+        private string PersonName { get; set; }
+        private string PersonSurname { get; set; }
+        private string PersonPatronymic { get; set; }
+        private DateTime PersonBirthday { get; set; }
+        private char PersonGender { get; set; }
+        private int PersonAge { get; set; }
+        public virtual int Age  
+        {
+            get => PersonAge;
+            set { if (value > 0 && value < 110) PersonAge = value; }
+        }
+        public Person(int id, string name, string surname, string patronymic, DateTime birthday, char gender, int age)    
+                                                                                                                          
+        {
+            PersonName = name;
+            PersonAge = age;
+            PersonSurname = surname;
+            PersonPatronymic = patronymic;
+            PersonBirthday = birthday;
+            PersonGender = gender;
+            IdPerson = id;
+        }
+        public string GetPersonName    
+        {
+            get
+            {
+                return PersonName;
+            }
+        }
+        public string SetPersonName    
+        {
+            set
+            {
+                PersonName = value;
+            }
+        }
+    }
+}
---
+++ Session_BolovFarkhat_3ISP9-14/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_BolovFarkhat_3ISP9_14
+{
+    internal class Student:Person
+    {
+        public Student(int id, string name, string surname, string patronymic, DateTime birthday, char gender, int age, int idGroup, string phoneNumber, string passportNumber, string email, int idScholarshipType) : base(id, name, surname, patronymic, birthday, gender, age)
+        {
+            this.idGroup = idGroup;
+            this.phoneNumber = phoneNumber;
+            this.passportNumber = passportNumber;
+            this.email = email;
+            this.idScholarshipType = idScholarshipType;
+        }
+        private int idGroup { get; set; } 
+        public int IdGroup
+        {
+            get { return idGroup; }
+        }
+        private int idScholarshipType { get; set; }
+        public int IdScholarshipType
+        {
+            get { return idScholarshipType; }
+        }
+        private string phoneNumber { get; set; }
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+        }
+        private string passportNumber { get; set; }
+        public string PassportNumber
+        {
+            get { return passportNumber; }
+        }
+
+        private string email { get; set; }
+        public string Email
+        {
+            get { return email; }
+        }
+    }
+}
